b54a2e0e375a3e05eeb90ece52f6eb8a86af78f4
Braulio Cancino Vera <brax.cancino@gmail.com>
2016-06-04 23:56:04 -0400

    add option menu

--- app/src/main/java/com/example/android/midicreator/CreateActivity.java
+++ app/src/main/java/com/example/android/midicreator/CreateActivity.java
@@ -1,9 +1,52 @@
 package com.example.android.midicreator;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(CreateActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.options_options:
+                intent = new Intent(CreateActivity.this, OptionsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.options_help:
+                intent = new Intent(CreateActivity.this, HelpActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.options_about:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/android/midicreator/OptionsActivity.java
+++ app/src/main/java/com/example/android/midicreator/OptionsActivity.java
@@ -1,9 +1,50 @@
 package com.example.android.midicreator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(OptionsActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.options_options:
+                intent = new Intent(OptionsActivity.this, OptionsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.options_help:
+                intent = new Intent(OptionsActivity.this, HelpActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.options_about:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
